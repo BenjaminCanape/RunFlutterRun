82dcefc6b90c7b00bf8b64ae6f9c6c5f5073daf8
Benjamin Canape <benjamin.canape@gmail.com>
2023-06-08 00:07:24 +0200

    translate remove alert

--- lib/presentation/activity_details/widgets/remove_alert.dart
+++ lib/presentation/activity_details/widgets/remove_alert.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:run_run_run/domain/entities/activity.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../view_model/activity_details_view_model.dart';
 
 class RemoveAlert extends HookConsumerWidget {
   final Activity activity;
 
-  const RemoveAlert({Key? key, required this.activity}) : super(key: key);
+  bool isLoading = false;
+
+  RemoveAlert({Key? key, required this.activity}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -15,12 +18,22 @@ class RemoveAlert extends HookConsumerWidget {
     final state = ref.watch(activityDetailsViewModelProvider);
     final provider = ref.read(activityDetailsViewModelProvider.notifier);
 
-    return AlertDialog(title: Text("Confirmer la suppression"), actions: [
-      TextButton(
-          onPressed: () => provider.removeActivity(activity, context),
-          child: Text("Supprimer")),
-      TextButton(
-          onPressed: () => Navigator.of(context).pop(), child: Text("Annuler"))
-    ]);
+    return AlertDialog(
+        title: isLoading
+            ? const CircularProgressIndicator()
+            : Text('${AppLocalizations.of(context).ask_activity_removal}'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                if (isLoading == false) {
+                  isLoading = true;
+                  provider.removeActivity(activity, context);
+                }
+              },
+              child: Text('${AppLocalizations.of(context).delete}')),
+          TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('${AppLocalizations.of(context).cancel}'))
+        ]);
   }
 }
