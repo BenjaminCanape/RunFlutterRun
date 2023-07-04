f76b91a31ffa3dd748845d9d009c58bd94a759d6
Benjamin Canape <benjamin.canape@gmail.com>
2023-07-04 23:28:34 +0200

    add title on nav bar

--- lib/presentation/home/screen/home_screen.dart
+++ lib/presentation/home/screen/home_screen.dart
@@ -7,6 +7,8 @@ import '../../new_activity/screen/new_activity_screen.dart';
 import '../../settings/screen/settings_screen.dart';
 import '../view_model/home_view_model.dart';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 enum Tabs { home, list }
 
 class HomeScreen extends HookConsumerWidget {
@@ -33,14 +35,19 @@ class HomeScreen extends HookConsumerWidget {
           locationViewModel.cancelLocationStream();
           homeViewModel.setCurrentIndex(value);
         },
-        showSelectedLabels: false,
+        showSelectedLabels: true,
         selectedItemColor: Colors.teal.shade700,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+        showUnselectedLabels: true,
+        items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: 'Settings'),
+              icon: const Icon(Icons.home),
+              label: AppLocalizations.of(context).activity),
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.list),
+              label: AppLocalizations.of(context).list),
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.settings),
+              label: AppLocalizations.of(context).settings),
         ],
       ),
     );
