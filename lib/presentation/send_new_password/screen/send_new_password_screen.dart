526257d9b01685266c19c4e0f64c2479bccd6c4d
Benjamin Canape <benjamin.canape@gmail.com>
2023-07-15 23:34:33 +0200

    add the forgottent password feature (#13)
    * add the forgottent password feature
    
    * fix MR

---
+++ lib/presentation/send_new_password/screen/send_new_password_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../common/core/utils/form_utils.dart';
+import '../../common/core/validators/login_validators.dart';
+import '../../common/core/utils/ui_utils.dart';
+import '../view_model/send_new_password_view_model.dart';
+
+class SendNewPasswordScreen extends HookConsumerWidget {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  /// Constructs a [SendNewPasswordScreen].
+  SendNewPasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(sendNewPasswordViewModelProvider);
+    final provider = ref.watch(sendNewPasswordViewModelProvider.notifier);
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade900,
+      body: state.isSending
+          ? const Center(child: UIUtils.loader)
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 0, top: 150),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Align(
+                        alignment: AlignmentDirectional.topStart,
+                        child: Text(
+                          AppLocalizations.of(context).send_new_password,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 33),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Form(
+                          key: formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                style: FormUtils.darkTextFormFieldStyle,
+                                cursorColor: Colors.teal.shade100,
+                                decoration: FormUtils.createInputDecorative(
+                                    AppLocalizations.of(context).email,
+                                    dark: true,
+                                    icon: Icons.email),
+                                validator: (value) =>
+                                    LoginValidators.email(context, value),
+                                onSaved: (value) {
+                                  provider.setEmail(value);
+                                },
+                              ),
+                              const SizedBox(height: 50),
+                              ElevatedButton(
+                                style: FormUtils.buttonStyle,
+                                onPressed: () {
+                                  provider.submitForm(context, formKey);
+                                },
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const Icon(Icons.send),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      AppLocalizations.of(context).send_mail,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              // Back Button
+                              ElevatedButton(
+                                style: FormUtils.buttonStyle,
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const Icon(Icons.arrow_back),
+                                    const SizedBox(width: 8),
+                                    Text(AppLocalizations.of(context).back),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
---
+++ lib/presentation/send_new_password/view_model/send_new_password_state.dart
@@ -0,0 +1,32 @@
+/// Represents the state of the send new password screen.
+class SendNewPasswordState {
+  /// The email entered by the user.
+  final String email;
+
+  /// Indicates whether the mail is sending
+  final bool isSending;
+
+  const SendNewPasswordState({
+    required this.email,
+    required this.isSending,
+  });
+
+  /// Creates an initial instance of [SendNewPasswordState].
+  factory SendNewPasswordState.initial() {
+    return const SendNewPasswordState(
+      email: '',
+      isSending: false,
+    );
+  }
+
+  /// Creates a copy of [SendNewPasswordState] with the specified fields replaced with new values.
+  SendNewPasswordState copyWith({
+    String? email,
+    bool? isSending,
+  }) {
+    return SendNewPasswordState(
+      email: email ?? this.email,
+      isSending: isSending ?? this.isSending,
+    );
+  }
+}
