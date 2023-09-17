93d637d499915594d4d73ae94703f0242014cc3e
Benjamin Canape <benjamin.canape@gmail.com>
2023-09-17 15:59:34 +0200

    add graph to activity details

--- lib/presentation/activity_details/screen/activity_details_screen.dart
+++ lib/presentation/activity_details/screen/activity_details_screen.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:quickalert/models/quickalert_type.dart';
 import 'package:quickalert/widgets/quickalert_dialog.dart';
+import 'package:run_flutter_run/presentation/activity_details/widgets/details_tab.dart';
+import 'package:run_flutter_run/presentation/activity_details/widgets/graph_tab.dart';
 
 import '../../../domain/entities/activity.dart';
-import '../../../domain/entities/enum/activity_type.dart';
 import '../../common/core/utils/activity_utils.dart';
 import '../../common/core/utils/ui_utils.dart';
-import '../../common/core/widgets/date.dart';
-import '../../common/core/widgets/share_map_button.dart';
-import '../../common/location/widgets/location_map.dart';
-import '../../common/metrics/widgets/metrics.dart';
-import '../../common/timer/widgets/timer_text.dart';
 import '../view_model/activity_details_view_model.dart';
 
 /// The screen that displays details of a specific activity.
@@ -29,59 +24,9 @@ class ActivityDetailsScreen extends HookConsumerWidget {
     final state = ref.watch(activityDetailsViewModelProvider);
     final provider = ref.read(activityDetailsViewModelProvider.notifier);
 
+    final tabController = useTabController(initialLength: 2);
+
     final displayedActivity = state.activity ?? activity;
-    ActivityType selectedType = state.type ?? displayedActivity.type;
-
-    // Calculate the points for the location map.
-    final List<LatLng> points = provider.savedPositionsLatLng(activity);
-    final List<Marker> markers = [];
-
-    // Add markers to the map if activity locations are available.
-    if (activity.locations.isNotEmpty) {
-      markers.add(
-        Marker(
-          width: 80.0,
-          height: 80.0,
-          point: LatLng(
-            activity.locations.first.latitude,
-            activity.locations.first.longitude,
-          ),
-          builder: (ctx) => Column(
-            children: [
-              IconButton(
-                icon: const Icon(Icons.location_on_rounded),
-                color: Colors.green.shade700,
-                iconSize: 35.0,
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-      );
-
-      if (activity.locations.length > 1) {
-        markers.add(
-          Marker(
-            width: 80.0,
-            height: 80.0,
-            point: LatLng(
-              activity.locations.last.latitude,
-              activity.locations.last.longitude,
-            ),
-            builder: (ctx) => Column(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.location_on_rounded),
-                  color: Colors.red,
-                  iconSize: 35.0,
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        );
-      }
-    }
 
     return Scaffold(
       body: state.isLoading
@@ -112,7 +57,10 @@ class ActivityDetailsScreen extends HookConsumerWidget {
                       IconButton(
                         color: Colors.black,
                         tooltip: 'Edit',
-                        onPressed: () => provider.editType(),
+                        onPressed: () {
+                          tabController.animateTo(0);
+                          provider.editType();
+                        },
                         icon: const Icon(
                           Icons.edit,
                           color: Colors.blueGrey,
@@ -142,66 +90,35 @@ class ActivityDetailsScreen extends HookConsumerWidget {
                       )
                     ]),
                   ),
-                  const Divider(),
-                  state.isEditing
-                      ? Column(children: [
-                          ActivityUtils.buildActivityTypeDropdown(
-                              context, selectedType, provider),
-                          const SizedBox(height: 20)
-                        ])
-                      : Container(),
-                  Date(date: displayedActivity.startDatetime),
-                  Column(
-                    children: [
-                      const SizedBox(height: 30),
-                      Center(
-                        child:
-                            TimerText(timeInMs: displayedActivity.time.toInt()),
-                      ),
-                      const SizedBox(height: 15),
-                      Metrics(
-                        distance: displayedActivity.distance,
-                        speed: displayedActivity.speed,
-                      ),
-                    ],
-                  ),
                   Expanded(
-                    child: RepaintBoundary(
-                      key: state.boundaryKey,
-                      child: LocationMap(points: points, markers: markers),
-                    ),
-                  )
+                      child: DefaultTabController(
+                          length: 2,
+                          child: Scaffold(
+                              appBar: TabBar(
+                                  controller: tabController,
+                                  labelColor: Colors.blueGrey,
+                                  dividerColor: Colors.blueGrey,
+                                  indicatorColor: Colors.blueGrey,
+                                  tabs: [
+                                    Tab(
+                                      text:
+                                          AppLocalizations.of(context).details,
+                                    ),
+                                    Tab(
+                                      text: AppLocalizations.of(context).graph,
+                                    ),
+                                  ]),
+                              body: TabBarView(
+                                  controller: tabController,
+                                  children: [
+                                    Expanded(
+                                        child: DetailsTab(activity: activity)),
+                                    Expanded(
+                                        child: GraphTab(activity: activity))
+                                  ]))))
                 ],
               ),
             ),
-      floatingActionButton: state.isEditing || state.isLoading
-          ? FloatingActionButton(
-              backgroundColor: Colors.teal.shade800,
-              elevation: 4.0,
-              onPressed: state.isLoading
-                  ? null
-                  : () {
-                      provider.save(displayedActivity);
-                    },
-              child: const Icon(Icons.save),
-            )
-          : Stack(
-              children: [
-                Positioned(
-                  bottom: 16,
-                  right: 80,
-                  child: ShareMapButton(
-                      activity: displayedActivity,
-                      boundaryKey: state.boundaryKey),
-                ),
-                const Positioned(
-                  bottom: 16,
-                  left: 80,
-                  child: BackButton(),
-                ),
-              ],
-            ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
