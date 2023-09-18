b9669f88e5dd083e4ae32cdaf3de2f07d03efead
Benjamin Canape <benjamin.canape@gmail.com>
2023-09-18 08:56:06 +0200

    fix no data on graph

--- lib/presentation/activity_details/widgets/graph_tab.dart
+++ lib/presentation/activity_details/widgets/graph_tab.dart
@@ -55,36 +55,40 @@ class GraphTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<FlSpot> spots = [];
-    final List<Location> locations = activity.locations.toList();
-    double totalDistance = 0;
 
-    for (int i = 1; i < locations.length; i++) {
-      final Location currentLocation = locations[i];
-      final Location previousLocation = locations[i - 1];
+    if (activity.locations.length > 1) {
+      final List<Location> locations = activity.locations.toList();
+      double totalDistance = 0;
+      for (int i = 1; i < locations.length; i++) {
+        final Location currentLocation = locations[i];
+        final Location previousLocation = locations[i - 1];
 
-      final double distance = MapUtils.getDistance(
-            LatLng(previousLocation.latitude, previousLocation.longitude),
-            LatLng(currentLocation.latitude, currentLocation.longitude),
-          ) /
-          1000;
+        final double distance = MapUtils.getDistance(
+              LatLng(previousLocation.latitude, previousLocation.longitude),
+              LatLng(currentLocation.latitude, currentLocation.longitude),
+            ) /
+            1000;
 
-      totalDistance += distance;
+        totalDistance += distance;
 
-      final Duration timeDifference =
-          currentLocation.datetime.difference(previousLocation.datetime);
+        final Duration timeDifference =
+            currentLocation.datetime.difference(previousLocation.datetime);
 
-      double hoursDifference;
+        double hoursDifference;
 
-      timeDifference.inSeconds == 0
-          ? hoursDifference = (timeDifference.inSeconds) / 3600
-          : hoursDifference = (timeDifference.inMilliseconds) / 3600000;
+        timeDifference.inSeconds == 0
+            ? hoursDifference = (timeDifference.inSeconds) / 3600
+            : hoursDifference = (timeDifference.inMilliseconds) / 3600000;
 
-      final double speed = hoursDifference > 0 ? distance / hoursDifference : 0;
-      spots.add(FlSpot(totalDistance, speed));
+        final double speed =
+            hoursDifference > 0 ? distance / hoursDifference : 0;
+        spots.add(FlSpot(totalDistance, speed));
+      }
     }
 
-    final smoothedData = smoothData(spots);
-    final maxSpeedSpot = getMaxSpeedSpot(smoothedData);
+    final smoothedData = spots.isNotEmpty ? smoothData(spots) : spots;
+    final maxSpeedSpot =
+        smoothedData.isNotEmpty ? getMaxSpeedSpot(smoothedData) : FlSpot(0, 0);
 
     return Scaffold(
       body: Column(
@@ -92,105 +96,113 @@ class GraphTab extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-              child: SizedBox(
-                height: 200,
-                child: LineChart(
-                  LineChartData(
-                    gridData: FlGridData(
-                        drawHorizontalLine: false,
-                        drawVerticalLine: true,
-                        verticalInterval: activity.distance > 2 ? 1 : 0.5),
-                    titlesData: FlTitlesData(
-                      leftTitles: AxisTitles(
-                        axisNameSize: 30,
-                        axisNameWidget: const Text('km/h',
-                            style: TextStyle(fontWeight: FontWeight.w900)),
-                        sideTitles: SideTitles(
-                          showTitles: true,
-                          reservedSize: 30,
-                          getTitlesWidget: (value, _) {
-                            if (value == 0 ||
-                                value ==
-                                    getMaxSpeedSpot(smoothedData).y * 1.25) {
-                              return const Text('');
-                            } else {
-                              return Text(value.toStringAsFixed(1));
-                            }
-                          },
+              child: smoothedData.isNotEmpty
+                  ? SizedBox(
+                      height: 200,
+                      child: LineChart(
+                        LineChartData(
+                          gridData: FlGridData(
+                              drawHorizontalLine: false,
+                              drawVerticalLine: true,
+                              verticalInterval:
+                                  activity.distance > 2 ? 1 : 0.5),
+                          titlesData: FlTitlesData(
+                            leftTitles: AxisTitles(
+                              axisNameSize: 30,
+                              axisNameWidget: const Text('km/h',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.w900)),
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                reservedSize: 30,
+                                getTitlesWidget: (value, _) {
+                                  if (value == 0 ||
+                                      value ==
+                                          getMaxSpeedSpot(smoothedData).y *
+                                              1.25) {
+                                    return const Text('');
+                                  } else {
+                                    return Text(value.toStringAsFixed(1));
+                                  }
+                                },
+                              ),
+                            ),
+                            rightTitles: const AxisTitles(
+                                sideTitles: SideTitles(showTitles: false)),
+                            topTitles: const AxisTitles(
+                                sideTitles: SideTitles(showTitles: false)),
+                            bottomTitles: AxisTitles(
+                                axisNameWidget: const Text('km',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w900)),
+                                sideTitles: SideTitles(
+                                  showTitles: true,
+                                  interval: activity.distance > 2 ? 1 : 0.5,
+                                  reservedSize: 30,
+                                  getTitlesWidget: (value, _) {
+                                    if (value == 0 ||
+                                        (activity.distance > 0.5 &&
+                                            value == activity.distance)) {
+                                      return const Text('');
+                                    } else {
+                                      return Text(value.toStringAsFixed(1));
+                                    }
+                                  },
+                                )),
+                          ),
+                          minX: 0,
+                          maxX: activity.distance,
+                          minY: 0,
+                          maxY: getMaxSpeedSpot(smoothedData).y * 1.25,
+                          showingTooltipIndicators: [],
+                          lineTouchData: LineTouchData(touchTooltipData:
+                              LineTouchTooltipData(
+                                  getTooltipItems: (touchedSpots) {
+                            return touchedSpots.map((LineBarSpot touchedSpot) {
+                              const textStyle = TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                              );
+                              return LineTooltipItem(
+                                '${smoothedData[touchedSpot.spotIndex].y.toStringAsFixed(2)} km/h',
+                                textStyle,
+                              );
+                            }).toList();
+                          })),
+                          lineBarsData: [
+                            LineChartBarData(
+                              spots: smoothedData,
+                              isCurved: true,
+                              dotData: const FlDotData(show: false),
+                              belowBarData: BarAreaData(show: false),
+                            ),
+                            LineChartBarData(
+                              spots: [maxSpeedSpot],
+                              isCurved: true,
+                              dotData: const FlDotData(show: true),
+                              belowBarData: BarAreaData(show: false),
+                            ),
+                          ],
+                          extraLinesData: ExtraLinesData(
+                            horizontalLines: [
+                              HorizontalLine(
+                                y: activity.speed,
+                                color: Colors.blueGrey,
+                                strokeWidth: 2,
+                                dashArray: [5, 5],
+                                label: HorizontalLineLabel(
+                                    show: true,
+                                    labelResolver: (_) =>
+                                        AppLocalizations.of(context)
+                                            .average_speed),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      rightTitles: const AxisTitles(
-                          sideTitles: SideTitles(showTitles: false)),
-                      topTitles: const AxisTitles(
-                          sideTitles: SideTitles(showTitles: false)),
-                      bottomTitles: AxisTitles(
-                          axisNameWidget: const Text('km',
-                              style: TextStyle(fontWeight: FontWeight.w900)),
-                          sideTitles: SideTitles(
-                            showTitles: true,
-                            interval: activity.distance > 2 ? 1 : 0.5,
-                            reservedSize: 30,
-                            getTitlesWidget: (value, _) {
-                              if (value == 0 ||
-                                  (activity.distance > 0.5 &&
-                                      value == activity.distance)) {
-                                return const Text('');
-                              } else {
-                                return Text(value.toStringAsFixed(1));
-                              }
-                            },
-                          )),
-                    ),
-                    minX: 0,
-                    maxX: activity.distance,
-                    minY: 0,
-                    maxY: getMaxSpeedSpot(smoothedData).y * 1.25,
-                    showingTooltipIndicators: [],
-                    lineTouchData: LineTouchData(touchTooltipData:
-                        LineTouchTooltipData(getTooltipItems: (touchedSpots) {
-                      return touchedSpots.map((LineBarSpot touchedSpot) {
-                        const textStyle = TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
-                        );
-                        return LineTooltipItem(
-                          '${smoothedData[touchedSpot.spotIndex].y.toStringAsFixed(2)} km/h',
-                          textStyle,
-                        );
-                      }).toList();
-                    })),
-                    lineBarsData: [
-                      LineChartBarData(
-                        spots: smoothedData,
-                        isCurved: true,
-                        dotData: const FlDotData(show: false),
-                        belowBarData: BarAreaData(show: false),
-                      ),
-                      LineChartBarData(
-                        spots: [maxSpeedSpot],
-                        isCurved: true,
-                        dotData: const FlDotData(show: true),
-                        belowBarData: BarAreaData(show: false),
-                      ),
-                    ],
-                    extraLinesData: ExtraLinesData(
-                      horizontalLines: [
-                        HorizontalLine(
-                          y: activity.speed,
-                          color: Colors.blueGrey,
-                          strokeWidth: 2,
-                          dashArray: [5, 5],
-                          label: HorizontalLineLabel(
-                              show: true,
-                              labelResolver: (_) =>
-                                  AppLocalizations.of(context).average_speed),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+                    )
+                  : Text(AppLocalizations.of(context).no_data),
             ),
           ),
         ],
