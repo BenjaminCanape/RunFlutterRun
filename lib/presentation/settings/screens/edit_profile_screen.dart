c8278b85d8f89ce9cc1926bf3287a8ab60136a82
Benjamin Canape <benjamin.canape@gmail.com>
2023-10-28 16:15:56 +0200

    improve edit profile feature (#24)

--- lib/presentation/settings/screens/edit_profile_screen.dart
+++ lib/presentation/settings/screens/edit_profile_screen.dart
@@ -12,109 +12,129 @@ class EditProfileScreen extends HookConsumerWidget {
 
   EditProfileScreen({Key? key}) : super(key: key);
 
+  final editProfileFutureProvider = FutureProvider<void>((ref) async {
+    final editProfileProvider =
+        ref.watch(editProfileViewModelProvider.notifier);
+    editProfileProvider.getCurrentUser();
+  });
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(editProfileViewModelProvider);
     final provider = ref.watch(editProfileViewModelProvider.notifier);
 
-    return Scaffold(
-      body: state.isEditing
-          ? const Center(child: UIUtils.loader)
-          : SafeArea(
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(left: 0, top: 12),
-                    child: Text(
-                      AppLocalizations.of(context)!.edit_profile,
-                      style: const TextStyle(
-                          color: Colors.blueGrey,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const Divider(),
-                  const SizedBox(height: 40),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Form(
-                      key: formKey,
-                      child: Column(
-                        children: [
-                          state.errorOnRequest
-                              ? Column(children: [
-                                  Text(
-                                    AppLocalizations.of(context)!
-                                        .edit_profile_error,
-                                    style: const TextStyle(
-                                        color: Colors.red, fontSize: 18),
-                                  ),
-                                  const SizedBox(height: 20)
-                                ])
-                              : Container(),
-                          // Firstname TextFormField
-                          TextFormField(
-                            style: FormUtils.textFormFieldStyle,
-                            decoration: FormUtils.createInputDecorative(
-                              AppLocalizations.of(context)!.firstname,
-                              dark: false,
-                              icon: Icons.person,
-                            ),
-                            validator: (value) =>
-                                LoginValidators.name(context, value),
-                            onSaved: (value) {
-                              provider.setFirstname(value);
-                            },
-                          ),
-                          // Lastname TextFormField
-                          TextFormField(
-                            style: FormUtils.textFormFieldStyle,
-                            decoration: FormUtils.createInputDecorative(
-                              AppLocalizations.of(context)!.lastname,
-                              dark: false,
-                              icon: Icons.person,
-                            ),
-                            validator: (value) =>
-                                LoginValidators.name(context, value),
-                            onChanged: (value) {
-                              provider.setLastname(value);
-                            },
-                          ),
-                        ],
+    var editProfileStateProvider = ref.watch(editProfileFutureProvider);
+
+    return editProfileStateProvider.when(
+      data: (_) {
+        return Scaffold(
+          body: state.isEditing
+              ? const Center(child: UIUtils.loader)
+              : SafeArea(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 0, top: 12),
+                        child: Text(
+                          AppLocalizations.of(context)!.edit_profile,
+                          style: const TextStyle(
+                              color: Colors.blueGrey,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
+                      const Divider(),
+                      const SizedBox(height: 40),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Form(
+                          key: formKey,
+                          child: Column(
+                            children: [
+                              state.errorOnRequest
+                                  ? Column(children: [
+                                      Text(
+                                        AppLocalizations.of(context)!
+                                            .edit_profile_error,
+                                        style: const TextStyle(
+                                            color: Colors.red, fontSize: 18),
+                                      ),
+                                      const SizedBox(height: 20)
+                                    ])
+                                  : Container(),
+                              // Firstname TextFormField
+                              TextFormField(
+                                style: FormUtils.textFormFieldStyle,
+                                decoration: FormUtils.createInputDecorative(
+                                  AppLocalizations.of(context)!.firstname,
+                                  dark: false,
+                                  icon: Icons.person,
+                                ),
+                                validator: (value) =>
+                                    LoginValidators.name(context, value),
+                                onSaved: (value) {
+                                  provider.setFirstname(value);
+                                },
+                                initialValue: state.firstname,
+                              ),
+                              // Lastname TextFormField
+                              TextFormField(
+                                style: FormUtils.textFormFieldStyle,
+                                decoration: FormUtils.createInputDecorative(
+                                  AppLocalizations.of(context)!.lastname,
+                                  dark: false,
+                                  icon: Icons.person,
+                                ),
+                                validator: (value) =>
+                                    LoginValidators.name(context, value),
+                                onChanged: (value) {
+                                  provider.setLastname(value);
+                                },
+                                initialValue: state.lastname,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
+          floatingActionButton: Stack(
+            children: [
+              Positioned(
+                bottom: 16,
+                right: 80,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.teal.shade800,
+                  elevation: 4.0,
+                  child: const Icon(Icons.save),
+                  onPressed: () {
+                    provider.submitForm(context, formKey);
+                  },
+                ),
               ),
-            ),
-      floatingActionButton: Stack(
-        children: [
-          Positioned(
-            bottom: 16,
-            right: 80,
-            child: FloatingActionButton(
-              backgroundColor: Colors.teal.shade800,
-              elevation: 4.0,
-              child: const Icon(Icons.save),
-              onPressed: () {
-                provider.submitForm(context, formKey);
-              },
-            ),
+              Positioned(
+                bottom: 16,
+                left: 80,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.teal.shade800,
+                  elevation: 4.0,
+                  child: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            bottom: 16,
-            left: 80,
-            child: FloatingActionButton(
-              backgroundColor: Colors.teal.shade800,
-              elevation: 4.0,
-              child: const Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ),
-        ],
-      ),
+        );
+      },
+      loading: () {
+        return const Center(child: UIUtils.loader);
+      },
+      error: (error, stackTrace) {
+        return Text('$error');
+      },
     );
   }
 }
