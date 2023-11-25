9a5304564e06a56db48ce25d79af2fa75f068561
Benjamin Canape <benjamin.canape@gmail.com>
2023-11-25 20:17:20 +0100

    group activities by month in list (#28)
    * group activities by month in list
    
    * edit screenshots
    
    * fix activity sort on empty list

--- lib/presentation/common/activity/widgets/activity_list.dart
+++ lib/presentation/common/activity/widgets/activity_list.dart
@@ -1,34 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
+import 'package:intl/intl.dart';
+import 'package:run_flutter_run/presentation/common/core/utils/type_utils.dart';
 import '../../../../domain/entities/activity.dart';
 import 'activity_item.dart';
 
-/// The screen that displays a list of activities.
+/// The screen that displays a list of activities grouped by months.
 class ActivityList extends HookConsumerWidget {
-  /// The activities to display.
   final List<Activity> activities;
-
-  /// display username ?
   final bool displayUserName;
-
-  /// can open activity ?
   final bool canOpenActivity;
 
-  /// Creates a [ActivityList] widget.
-  ///
-  /// The [activities] is the activities to display.
-  const ActivityList(
-      {super.key,
-      this.displayUserName = false,
-      this.canOpenActivity = true,
-      required this.activities});
+  const ActivityList({
+    Key? key,
+    this.displayUserName = false,
+    this.canOpenActivity = true,
+    required this.activities,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final groupedActivities = groupActivitiesByMonth(activities);
+
     return Expanded(
-      child: activities.isEmpty
+      child: groupedActivities.isEmpty
           ? Center(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -41,22 +37,81 @@ class ActivityList extends HookConsumerWidget {
                   Text(
                     AppLocalizations.of(context)!.no_data,
                     style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
             )
-          : ListView.builder(
-              itemCount: activities.length,
-              itemBuilder: (context, index) {
-                return ActivityItem(
-                  index: index,
-                  activity: activities[index],
-                  displayUserName: displayUserName,
-                  canOpenActivity: canOpenActivity,
-                );
-              },
+          : Theme(
+              data: ThemeData(
+                expansionTileTheme: const ExpansionTileThemeData(
+                  tilePadding: EdgeInsets.zero,
+                  iconColor: Colors.black,
+                  textColor: Colors.black87,
+                  childrenPadding: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide.none,
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+              ),
+              child: ListView.builder(
+                itemCount: groupedActivities.length,
+                itemBuilder: (context, monthIndex) {
+                  final monthActivities = groupedActivities[monthIndex];
+
+                  return ExpansionTile(
+                    tilePadding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                    title: Text(
+                      '${_getMonthName(monthActivities.first.startDatetime, AppLocalizations.of(context)!.localeName)} ${monthActivities.first.startDatetime.year}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    initiallyExpanded: true,
+                    children: monthActivities.map((activity) {
+                      return ActivityItem(
+                        index: activities.indexOf(activity),
+                        activity: activity,
+                        displayUserName: displayUserName,
+                        canOpenActivity: canOpenActivity,
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
             ),
     );
+  }
+
+  List<List<Activity>> groupActivitiesByMonth(List<Activity> activities) {
+    final groupedActivities = <List<Activity>>[];
+
+    if (activities.isNotEmpty) {
+      activities.sort((a, b) => b.startDatetime.compareTo(a.startDatetime));
+
+      for (var activity in activities) {
+        if (groupedActivities.isEmpty ||
+            groupedActivities.last.first.startDatetime.month !=
+                activity.startDatetime.month) {
+          groupedActivities.add([activity]);
+        } else {
+          groupedActivities.last.add(activity);
+        }
+      }
+    }
+    return groupedActivities;
+  }
+
+  String _getMonthName(DateTime dateTime, String locale) {
+    final monthNameFormat = DateFormat('MMMM', locale);
+
+    try {
+      return monthNameFormat.format(dateTime).capitalize();
+    } catch (e) {
+      return '';
+    }
   }
 }
